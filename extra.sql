e6f847ce0a577d8c1426245234d53d9d0b521cd0
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-04-12 11:41:17 -0700

    Update extra.sql

--- extra.sql
+++ extra.sql
@@ -12,4 +12,4 @@ CREATE TABLE schedule (
     FOREIGN KEY (roomid) REFERENCES room(id),
     FOREIGN KEY (timeslotid) REFERENCES timeslots(id));
   
-ALTER TABLE schedule ADD CONSTRAINT unique_room_time UNIQUE (roomid, timeslotid);
+ALTER TABLE schedule CONSTRAINT unique_room_time UNIQUE (roomid, timeslotid);
