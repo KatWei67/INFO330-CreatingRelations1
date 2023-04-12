fea76e13a4ec6642289dc66dec38dec8915d9393
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-04-12 15:12:36 -0700

    Update buildings.sql

--- buildings.sql
+++ buildings.sql
@@ -1,11 +1,11 @@
-CREATE TABLE buildings (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(80) NOT NULL,
-    shortname VARCHAR(10) UNIQUE );
+CREATE TABLE buildings (id INTEGER PRIMARY KEY, name VARCHAR(80) NOT NULL, shortname VARCHAR(10) UNIQUE);
 
-CREATE TABLE rooms (
-    id INTEGER PRIMARY KEY, --extra
-    number INTEGER,
-    buildingid INTEGER,
-    seating INTEGER,
-    FOREIGN KEY (buildingid) REFERENCES buildings(id));
+CREATE TABLE rooms (id INTEGER PRIMARY KEY, --extra 
+                    number INTEGER, buildingid INTEGER, seating INTEGER, FOREIGN KEY (buildingid) REFERENCES buildings(id));
+
+INSERT INTO buildings(id, name, shortname)
+VALUES (1, 'Mary Gate Hall', 'MGH'), (2, 'Smith Hall', 'SMI'), (3, 'Raitt Hall', 'RAI');
+
+
+INSERT INTO rooms(id, number, buildingid, seating)
+VALUES (1, 115, 2, 30), (2, 413, 1, 45), (3, 305, 3, 20);
