2e8b50b00a56cd4bb46b312f7b18de156b8a42cf
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-04-12 08:23:43 -0700

    Update courses.sql

--- courses.sql
+++ courses.sql
@@ -7,8 +7,8 @@ CREATE TABLE courses (
     PRIMARY KEY (code));
 INSERT INTO courses (code, start, end, description) VALUES
     ('INFO330A', '2023-04-01', '2023-06-01', 'Data and databases'),
-    ('INFO314 ', '2023-04-01', '2023-06-01', 'Networking and distributed Systems'),
+    ('INFO3140', '2023-04-01', '2023-06-01', 'Networking and distributed Systems'),
     ('INFO448A', '2023-09-25', '2023-12-19', 'Introduction to iOS'),
     ('INFO449A', '2023-09-25', '2023-12-19', 'Introduction to Android'),
-    ('BAW010 ', '2023-04-01', '2023-06-01', 'Introduction to Basket-Weaving'),
+    ('BAW0100', '2023-04-01', '2023-06-01', 'Introduction to Basket-Weaving'),
     ('BAW100A', '2023-04-01', '2023-06-01', 'Underwater Basket-Weaving');
