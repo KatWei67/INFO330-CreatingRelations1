002b6a1b50a314530a82c1c47ef09458add9b927
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-04-12 08:13:07 -0700

    Create courses.sql

---
+++ courses.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS courses;
+CREATE TABLE courses (
+    code VARCHAR(40) NOT NULL CHECK (LENGTH(code) >= 7),
+    description VARCHAR(400),
+    start DATE NOT NULL,
+    end DATE NOT NULL CHECK (end > start),
+    PRIMARY KEY (code));
+INSERT INTO courses (code, start, end, description) VALUES
+    ('INFO330A', '2023-04-01', '2023-06-01', 'Data and databases'),
+    ('INFO314 ', '2023-04-01', '2023-06-01', 'Networking and distributed Systems'),
+    ('INFO448A', '2023-09-25', '2023-12-19', 'Introduction to iOS'),
+    ('INFO449A', '2023-09-25', '2023-12-19', 'Introduction to Android'),
+    ('BAW010 ', '2023-04-01', '2023-06-01', 'Introduction to Basket-Weaving'),
+    ('BAW100A', '2023-04-01', '2023-06-01', 'Underwater Basket-Weaving');
