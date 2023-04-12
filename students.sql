560b71a5253af7c337f667280c28d9f0e1749586
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-04-12 07:42:52 -0700

    Update students.sql

--- students.sql
+++ students.sql
@@ -3,4 +3,4 @@ CREATE TABLE students(id INTEGER PRIMARY KEY, firstname VARCHAR(40), lastname VA
 INSERT INTO students (id, firstname, lastname, age) 
 VALUES (1, 'Fred', 'Flintstone',  35), ( 2, 'Wilma', 'Flintstone',  29), (3, 'Barney', 'Rubble', 33), (4, 'Betty', 'Rubble', 29), (5, 'Pebbles', 'Flintstone', 1), (6, 'Bam-Bam', 'Rubble', 1);
 INSERT INTO students (id, firstname, lastname, age) 
-VALUES (7, 'Katherine', 'Wei', 3),
+VALUES (7, 'Linda', 'Wong', 30), (8, 'Yue', 'Liong', 27), (9, 'Lora', 'Heong', 23), (10, 'Feasd', 'Ndaj', 7);
