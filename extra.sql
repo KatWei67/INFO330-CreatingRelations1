6ca2b4f30c5a954f2402f432c128cf6acde75491
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-04-12 11:07:07 -0700

    Update extra.sql

--- extra.sql
+++ extra.sql
@@ -2,12 +2,16 @@ CREATE TABLE timeslots (
     id INTEGER PRIMARY KEY,
     start TIME NOT NULL,
     end TIME NOT NULL);
+ 
 
-INSERT INTO timeslots (id, start, end)
-VALUES
-    ('INFO330A ', '2023-04-01', '2023-06-01'),
-    ('INFO3140', '2023-04-01', '2023-06-01'),
-    ('INFO448A', '2023-09-25', '2023-12-19'),
-    ('INFO449A', '2023-09-25', '2023-12-19'),
-    ('BAW0100', '2023-04-01', '2023-06-01'),
-    ('BAW100A', '2023-04-01', '2023-06-01');
+CREATE TABLE schedule (
+    coursecode TEXT NOT NULL,
+    roomid INTEGER NOT NULL,
+    timeslotid INTEGER NOT NULL,
+    FOREIGN KEY (coursecode) REFERENCES courses(code),
+    FOREIGN KEY (roomid) REFERENCES room(id),
+    FOREIGN KEY (timeslotid) REFERENCES timeslots(id));
+  
+ALTER TABLE schedule
+ADD CONSTRAINT unique_room_time
+UNIQUE (roomid, timeslotid);
